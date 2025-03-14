05b443581946ac5ae4a709afa2476b64a7aaa080
Bertrand Marchand <bertrand.marchand@usherbrooke.ca>
2025-03-14 15:48:00 -0400

    a wrong degen based solver

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::io::BufRead;
 use rsmaxsat::solver::MaxSATSolver;
+use std::collections::VecDeque;
 
 #[derive(Default,Debug)]
 pub struct Graph {
@@ -15,6 +16,28 @@ impl Graph {
     fn add_edge(&mut self, u: usize, v: usize) {
         self.ngbh.entry(u).or_insert_with(Vec::new).push(v);
         self.ngbh.entry(v).or_insert_with(Vec::new).push(u);
+    }
+
+    fn nnodes(&self) -> usize {
+        return self.ngbh.len();
+    }
+
+    fn find_min_degree_vertex(&self) -> Option<(usize,usize)> {
+        self.ngbh
+            .iter()
+            .min_by_key(|(_, vec)| vec.len())
+            .map(|(&key, vec)| (key, vec.len()))
+    }
+
+    fn delete_node(&mut self, node_to_delete: usize) {
+        self.ngbh.remove_entry(&node_to_delete);
+        for vec in self.ngbh.values_mut() {
+            vec.retain(|&x| x != node_to_delete);
+        }
+    }
+
+    fn neighborhood(&self, u: usize) -> Vec<usize> {
+        self.ngbh.get(&u).unwrap().to_vec()
     }
 }
 
@@ -59,6 +82,71 @@ pub fn parse_graph() -> Graph {
         graph.add_edge(a,b);
     }
         return graph;
+}
+
+pub fn degeneracy(mut graph: Graph) -> (VecDeque<usize>, usize) {
+
+    let mut degeneracy: usize = 0;
+    let mut order: VecDeque<usize> = VecDeque::new();
+    
+    while graph.nnodes() > 0 {
+        // finding min node
+        let (node_to_elim, min_degree) = graph.find_min_degree_vertex().unwrap();
+        //println!("node {:?} degree {:?}", node_to_elim, min_degree);
+    
+        // updating degeneracy
+        if min_degree > degeneracy {
+            degeneracy = min_degree;
+        }
+    
+        graph.delete_node(node_to_elim);
+        order.push_back(node_to_elim);
+    }
+    
+    return (order, degeneracy);
+    
+}
+
+pub fn degen_solver(graph: Graph, mut order: VecDeque<usize>, depth: usize) -> Vec<usize> {
+
+    let mut ds: Vec<usize> = Vec::new();
+
+    if graph.nnodes() > 0 {
+    
+        let mut u = order.pop_front().unwrap();
+        while !graph.ngbh.contains_key(&u) {
+            u = order.pop_front().unwrap();
+        }
+        println!("branching over {:?} at depth {:?}", u, depth);
+
+        let mut test_set: Vec<usize> = vec![u];
+        for v in graph.neighborhood(u) {
+            test_set.push(v);
+        }
+
+        let first_round: bool = true;
+
+        for w in test_set {
+            let mut graph_w = Graph { ngbh: graph.ngbh.clone() };
+            for x in graph.neighborhood(w) {
+                graph_w.delete_node(x);
+            }
+            graph_w.delete_node(w);
+            let mut ds_w: Vec<usize> = degen_solver(graph_w, order.clone(), depth+1);
+            ds_w.push(w);
+
+            if first_round {
+                ds = ds_w;
+            }
+            else {
+                if ds.len() > ds_w.len() {
+                    ds = ds_w
+                }
+            }
+        }
+    }
+
+    return ds;
 }
 
 pub fn max_sat_solver(graph: Graph) -> Vec<bool> {
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use pace_2025_solver::*;
-use rsmaxsat::solver::MaxSATSolver;
+//use rsmaxsat::solver::MaxSATSolver;
 
 fn main() {
     let graph: Graph = parse_graph();
 
-    let sat_solution = max_sat_solver(graph); 
+    let (order, degeneracy) = degeneracy(Graph { ngbh: graph.ngbh.clone()});
+    println!("degeneracy {:?}", degeneracy);
 
-    // counting true.
-    let mut n: usize = 0;
+    let ds = degen_solver(graph, order,0);
 
-    for b in &sat_solution {
-        if *b {
-            n += 1;
-        }
-    }
-    println!("{:?}", n);
-    for k in 1..=sat_solution.len() {
-        if sat_solution[k-1] {
-            println!("{:?}",k);
-        }
-    }
+    println!("min DS size: {:?}, DS: {:?}", ds.len(), ds);
+
+//    let sat_solution = max_sat_solver(graph); 
+//
+//    // counting true.
+//    let mut n: usize = 0;
+//
+//    for b in &sat_solution {
+//        if *b {
+//            n += 1;
+//        }
+//    }
+//    println!("{:?}", n);
+//    for k in 1..=sat_solution.len() {
+//        if sat_solution[k-1] {
+//            println!("{:?}",k);
+//        }
+//    }
 }
